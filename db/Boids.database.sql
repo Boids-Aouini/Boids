7367656a667dd83f153130c54ff6ccec418e932c
aminaouini <aminaouini565@gmail.com>
2020-05-01 03:31:19 +0100

    create table users in sql file

--- db/Boids.database.sql
+++ db/Boids.database.sql
@@ -2,3 +2,17 @@ CREATE DATABASE Boids;
 
 USE Boids;
 
+CREATE TABLE Users (
+    id int NOT NULL UNIQUE AUTO_INCREMENT,
+    firstname varchar(30) NOT NULL,
+    lastname varchar(30) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    birthDate Date,
+    longitude int,
+    latitude int,
+    heighAccuracy int,
+    updatedAt Date,
+    createdAt Date,
+    PRIMARY KEY (id)
+);
